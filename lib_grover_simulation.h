603b9ef0c6bdae7bde1f7a01d58c1398cad47ad1
berthou <vincent@berthou>
2012-06-27 18:56:49 +0900

    lib header file creation

---
+++ lib_grover_simulation.h
@@ -0,0 +1,16 @@
+#ifndef LIB_GROVER_SIMULATION_CC
+#define LIB_GROVER_SIMULATION_CC
+
+#define FACTORIAL(result,n) 		\
+int i;				\
+result = 1;			\
+  for (i = 1; i <= n; i++)	\
+    result = result * i;
+
+#define INNER_PRODUCT(k,kx)
+float result;
+int i
+for(
+
+
+#endif

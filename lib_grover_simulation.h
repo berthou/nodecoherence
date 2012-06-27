300fbead912cb29485154188a01f8385f3c2774c
berthou <vincent@berthou>
2012-06-27 21:12:37 +0900

    inner_product

--- lib_grover_simulation.h
+++ lib_grover_simulation.h
@@ -1,16 +1,47 @@
 #ifndef LIB_GROVER_SIMULATION_CC
 #define LIB_GROVER_SIMULATION_CC
 
-#define FACTORIAL(result,n) 		\
-int i;				\
-result = 1;			\
-  for (i = 1; i <= n; i++)	\
-    result = result * i;
+#define FACTORIAL(x) 		\
+	do {					\
+		if(x<=1)			\
+		x=1.0;			\
+		else				\
+		for(i=x-1;i>1;i--){x=x*i;} \
+	}while(0)
 
-#define INNER_PRODUCT(k,kx)
-float result;
-int i
-for(
+float
+inner_product(float k,float kx,int np)
+{
+	int i,min,max;
+	float result=0,
+		  k_fact,
+		  npk_fact,
+		  kx_fact,
+		  npkx_fact,
+		  kn_fact,
+		  npkxn_fact,
+		  n_fact,
+		  nkxk_fact;
+
+	FACTORIAL(k_fact);
+	FACTORIAL(npk_fact);
+	FACTORIAL(kx_fact);
+	FACTORIAL(npkx_fact);
+	FACTORIAL(kn_fact);
+	FACTORIAL(npkxn_fact);
+	FACTORIAL(n_fact);
+	FACTORIAL(nkxk_fact);  
+
+	min = (k-kx > 0) ? k-kx : 0; 
+	max = (np-kx > k) ? k : np-k;
+	printf("%d %d\n",min,max);
+	for(i=min;i<=max;i++){ 
+		result+=pow(-1,i)/(kn_fact*npkxn_fact*n_fact*nkxk_fact);
+	}
+	result*=sqrt((k_fact*npk_fact*kx_fact*npkx_fact)/pow(2,np));
+return result;
+}
+
 
 
 #endif

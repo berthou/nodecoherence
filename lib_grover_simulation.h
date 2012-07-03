a4d51e2fa8db0b4929ed36544071d2c17ff58036
berthou <vincent@berthou>
2012-07-03 22:13:02 +0900

    update

--- lib_grover_simulation.h
+++ lib_grover_simulation.h
@@ -1,23 +1,110 @@
 #ifndef LIB_GROVER_SIMULATION_CC
 #define LIB_GROVER_SIMULATION_CC
 
+#define COMPUTE_COMBFACTOR(Np,statens,M,var)											\
+	do {																				\
+		var = 1/(sqrt(pow(2,Np*M)));													\
+		combfactor_product=1;													\
+		for(combfactor_iterator=0;combfactor_iterator<M;combfactor_iterator++) \
+		{ 																		\
+			BINOMIAL_COEFF(Np,statens[combfactor_iterator],combfactor_bin);			\
+			combfactor_product*=combfactor_bin;										\
+		}																		\
+		combfactor_product=sqrt(combfactor_product);								\
+		var*=combfactor_product;												\
+	}while(0)
+
+
+
+#define BINOMIAL_COEFF(n,k,var)				\
+	do {										\
+		var = 1;								\
+		if(n-2*k>0)								\
+		{										\
+			for(binomial_iterator=n;binomial_iterator>=n-k+1;binomial_iterator--)				\
+			var=var*binomial_iterator/(n-binomial_iterator+1);		\
+		}										\
+		else									\
+		{										\
+			for(binomial_iterator=n;binomial_iterator>=k+1;binomial_iterator--)						\
+			var=var*binomial_iterator/(n-binomial_iterator+1);			\
+		}											\
+	}while(0)
+
+float binomial_coeff(int n, int k)
+{
+	float result = 1;
+	int i;
+	if(n-2*k>0)
+	{
+		for(i=n;i>=n-k+1;i--)
+			result=result*i/(n-i+1);
+	}
+	else
+	{
+		for(i=n;i>=k+1;i--)
+			result=result*i/(n-i+1);
+	}
+	return result;
+}
+
+int *compute_state_list(int number,int base, int array_length)
+{
+	/* Convert number 'number' in base 'base'
+	 * the result is stored in an array of size 'array_length'
+	 * with LSB representation
+	 */
+
+	// Allocate result array
+	int *result = (int*)malloc(array_length*sizeof(int));
+
+	// Start to fill from the end of the array :
+	int index=array_length-1;
+
+	while(number != 0)
+	{
+		result[index] = (number % base);
+		number= (number / base);
+		index--;
+	}
+	return result;
+}
+
+size_t compute_matrix_size(n,p)
+{
+	/*
+	 * Computes n^p and return the result as a size_t
+	 */
+	int i;
+	size_t result=n;
+	for(i=1;i<p;i++) {
+		result*=n;
+	}
+	return result;
+}
+
 #define FACTORIAL(x) 		\
 	do {					\
 		if(x<=1)			\
 		x=1.0;			\
 		else				\
-		for(i=x-1;i>1;i--){x=x*i;} \
+		for(fact_iterator=x-1;fact_iterator>1;fact_iterator--){x=x*fact_iterator;} \
 	}while(0)
 
-float
-inner_product(float k,float kx,int np)
+
+float inner_product(float k,float kx,int np)
 {
-	int i,min,max;
+	// fact_iterator is used in the FACTORIAL macro
+	int i,
+		start,
+		stop,
+		fact_iterator;
+
 	float result=0,
-		  k_fact,
-		  npk_fact,
-		  kx_fact,
-		  npkx_fact,
+		  k_fact    = k,
+		  npk_fact  = np-k,
+		  kx_fact   = kx,
+		  npkx_fact = np-kx,
 		  kn_fact,
 		  npkxn_fact,
 		  n_fact,
@@ -27,21 +114,32 @@ inner_product(float k,float kx,int np)
 	FACTORIAL(npk_fact);
 	FACTORIAL(kx_fact);
 	FACTORIAL(npkx_fact);
-	FACTORIAL(kn_fact);
-	FACTORIAL(npkxn_fact);
-	FACTORIAL(n_fact);
-	FACTORIAL(nkxk_fact);  
 
-	min = (k-kx > 0) ? k-kx : 0; 
-	max = (np-kx > k) ? k : np-k;
-	printf("%d %d\n",min,max);
-	for(i=min;i<=max;i++){ 
-		result+=pow(-1,i)/(kn_fact*npkxn_fact*n_fact*nkxk_fact);
+	/* start = max(k-kx)
+	 * stop = min(k,Np-kx)
+	 */
+	start = (k-kx > 0) ? k-kx : 0; 
+	stop = (k <= np-kx) ? k : np-kx;
+
+	// Sum
+	for(i=start;i<=stop;i++){ 
+
+		kn_fact=k-i;
+		npkxn_fact=np-kx-i;
+		n_fact=i;
+		nkxk_fact=i+kx-k;
+
+		FACTORIAL(kn_fact);
+		FACTORIAL(npkxn_fact);
+		FACTORIAL(n_fact);
+		FACTORIAL(nkxk_fact);  
+
+		result+=(pow(-1,i)/(kn_fact*npkxn_fact*n_fact*nkxk_fact));
 	}
+	// Sum * sqrt
 	result*=sqrt((k_fact*npk_fact*kx_fact*npkx_fact)/pow(2,np));
-return result;
+
+	return result;
 }
-
-
 
 #endif
--- main.c
+++ main.c
@@ -1,18 +1,66 @@
 #include <stdio.h>
 #include <math.h>
-#include <stdlib.h>
+//#include <stdlib.h>
+
+#include <gsl/gsl_matrix.h>
 
 #include "lib_grover_simulation.h"
 
 int main(int argc, char **argv)
 {
-	float res = inner_product(10,10,10);
-	printf("%.20f\n",res);
-	/*int i;
-	for(i=0;i<=10;i++){ 
-		printf("%f ",2.0 << i);
+	/* number of bosons : M
+	 * number of sites  : Np
+	 * computing from Np_start to Np_stop (arguments from the command line)
+	 * The GNU Scientific Library is used for operations on matrices
+	 * rows_iterator and columns_iterator and used to run through the matrix H.
+	 */
+
+	int M=atoi(argv[3]),
+		Np,
+		Np_start=atoi(argv[1]),
+		Np_stop=atoi(argv[2]),
+		rows_iterator,
+		columns_iterator,
+		combfactor_iterator,
+		binomial_iterator;
+
+
+	int *statens;
+	double *combfactor,
+		   combfactor_var,
+		   combfactor_bin,
+		   combfactor_product;
+
+	for(Np=Np_start;Np<=Np_stop;Np++) {
+		printf("Np = %d\n",Np);
+		// Get maxtrix size
+		size_t matrix_size = compute_matrix_size(Np+1,M);
+		// Allocate matrix filled of 0s
+		gsl_matrix *H = gsl_matrix_calloc(matrix_size,matrix_size);
+		// Allocate combfactor array
+		combfactor = (double*)malloc(matrix_size*sizeof(double));
+		// Iteration over rows of the matrix
+		for(rows_iterator=0;rows_iterator<matrix_size;rows_iterator++) {
+			statens = compute_state_list(rows_iterator,Np+1,M);
+			COMPUTE_COMBFACTOR(Np,statens,M,combfactor[rows_iterator]);
+			printf("combfactor[%d] = %.10f\n",rows_iterator,combfactor[rows_iterator]);
+			/*
+			 *
+			 *
+			 */
+			for(columns_iterator=0;columns_iterator<matrix_size;columns_iterator++) {
+				/*
+				 *
+				 *
+				 */
+
+				//free(statens)
+				//free(combfactor)
+			}
+		}
+
+		//gsl_matrix_free(H);
 	}
-	*/
 	return 0;
 }
 

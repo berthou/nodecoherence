abc29f770fbf1a33d5c67bda0de33cbe5ddb92ad
berthou <vincent@berthou>
2012-07-03 23:31:24 +0900

    diagonal done

--- main.c
+++ main.c
@@ -12,7 +12,10 @@ int main(int argc, char **argv)
 	 * number of sites  : Np
 	 * computing from Np_start to Np_stop (arguments from the command line)
 	 * The GNU Scientific Library is used for operations on matrices
-	 * rows_iterator and columns_iterator and used to run through the matrix H.
+	 * rows_iterator and columns_iterator and used to run through the matrix H
+	 * binomial_iterator and combfactor_iterator are respectively used in the BINOMIAL_COEFF and COMPUTE_COMBFACTOR macros as iterators
+	 * combfactor_var, combfactor_bin, combfactor_product are variables of the COMPUTE_COMBFACTOR macro
+	 * diagonal_iterator is used as iterator to compute the diagonal-specific product 
 	 */
 
 	int M=atoi(argv[3]),
@@ -22,14 +25,19 @@ int main(int argc, char **argv)
 		rows_iterator,
 		columns_iterator,
 		combfactor_iterator,
-		binomial_iterator;
+		binomial_iterator,
+		diagonal_iterator;
 
 
-	int *statens;
+	int *statens,
+		*statensp;
+	
 	double *combfactor,
 		   combfactor_var,
 		   combfactor_bin,
-		   combfactor_product;
+		   combfactor_product,
+		   diagonal_product,
+		   *szmatelem;
 
 	for(Np=Np_start;Np<=Np_stop;Np++) {
 		printf("Np = %d\n",Np);
@@ -39,26 +47,39 @@ int main(int argc, char **argv)
 		gsl_matrix *H = gsl_matrix_calloc(matrix_size,matrix_size);
 		// Allocate combfactor array
 		combfactor = (double*)malloc(matrix_size*sizeof(double));
+		szmatelem  = (double*)malloc(matrix_size*sizeof(double));
 		// Iteration over rows of the matrix
 		for(rows_iterator=0;rows_iterator<matrix_size;rows_iterator++) {
 			statens = compute_state_list(rows_iterator,Np+1,M);
 			COMPUTE_COMBFACTOR(Np,statens,M,combfactor[rows_iterator]);
-			printf("combfactor[%d] = %.10f\n",rows_iterator,combfactor[rows_iterator]);
-			/*
-			 *
-			 *
-			 */
+			szmatelem[rows_iterator]=(2.0*statens[0]-Np)/Np;	
+
 			for(columns_iterator=0;columns_iterator<matrix_size;columns_iterator++) {
-				/*
-				 *
-				 *
-				 */
+				statensp = compute_state_list(columns_iterator,Np+1,M);
+				
+				// If we are on the diagonal of H :
+				if (rows_iterator == columns_iterator) {
+						diagonal_product=1.0;
+					for(diagonal_iterator=0;diagonal_iterator<M;diagonal_iterator++) {
+						diagonal_product*=(float)statens[diagonal_iterator]/(float)Np;
+					}
+					gsl_matrix_set(H,rows_iterator,columns_iterator,diagonal_product);
+					//printf("%f ",diagonal_product/*gsl_matrix_get(H,rows_iterator,rows_iterator*/);
+				}
+				// For every element of the matrix :
+					//gsl_matrix_set(H,rows_iterator,columns_iterator,
 
 				//free(statens)
 				//free(combfactor)
 			}
 		}
-
+		/*
+		int i;
+		for(i=0;i<M;i++) {
+			printf("statensp[%d] = %d\n",i,statensp[i]);
+			printf("statens[%d] = %d\n",i,statens[i]);
+		}
+		*/
 		//gsl_matrix_free(H);
 	}
 	return 0;
